a562669b5937ded34ca9e24935039628e1840a98
Jose <jccb.15@gmail.com>
2023-08-31 18:39:31 +0900

    adjust finetune parameters for colab

--- config/finetune_amlo.py
+++ config/finetune_amlo.py
@@ -8,7 +8,7 @@ wandb_project = 'amlo'
 wandb_run_name = 'ft-' + str(time.time())
 
 dataset = 'amlo'
-init_from = 'gpt2-xl' # this is the largest GPT-2 model
+init_from = 'gpt2-medium' # this is the largest GPT-2 model
 
 # only save checkpoints if the validation loss improves
 always_save_checkpoint = False
@@ -18,8 +18,10 @@ always_save_checkpoint = False
 # shakespeare has 301,966 tokens, so 1 epoch ~= 9.2 iters
 batch_size = 1
 gradient_accumulation_steps = 32
-max_iters = 20
+max_iters = 10000
 
 # finetune at constant LR
-learning_rate = 3e-5
-decay_lr = False
+learning_rate = 1e-4
+min_lr = 3e-5
+decay_lr = True
+lr_decay_iters = 10000
